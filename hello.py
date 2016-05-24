204a4fe67fe88b9de23ac6b10791b5e206ee3f49
kitaro <kitaro_tus@yahoo.co.jp>
2016-05-24 12:38:39 +0900

    とりあえず
    書いてみた。

---
+++ hello.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+print("Hello", "World!")
+
+x = "blue"
+y = "green"
+z = x
+
+print(x, y, z)
+
+z = y
+
+print(x, y, z)
+
+x = z
+
+print(x, y, z)
+
+
+route = 865
+print(route, type(route)) #865 <class 'int'> と表示
+route = "North"
+print(route, type(route)) #North- <class 'str'> と表示
+
+[1, 4, 9, 16, 25, 36, 49]
+['alpha', 'bravo', 'charlie', 'delta', 'echo']
+['zebra', 49]
