9fe5a7c7784d8549a03a640362eae31c6d525f39
kitaro <kitaro_tus@yahoo.co.jp>
2016-05-24 21:58:08 +0900

    整理
    エラー部分を整理。

--- hello.py
+++ hello.py
@@ -22,23 +22,14 @@ print(route, type(route)) #865 <class 'int'> と表示
 route = "North"
 print(route, type(route)) #North- <class 'str'> と表示
 
-[1, 4, 9, 16, 25, 36, 49]
-['alpha', 'bravo', 'charlie', 'delta', 'echo']
-['zebra', 49. -879. 'aardvark', 200]
-[]
 
-
-if boolean_expression1:
-    suite1
-elif boolean_expression2:
-    suite2
-elif boolean_expressionN:
-    suiteN
-else:
-    else_suite
 
 if x:
     print("x is nonzero")
+
+
+lines = 50000
+
 if lines < 1000:
     print("small")
 elif lines < 10000:
@@ -47,24 +38,11 @@ else:
     print("large")
 
 
-while boolean_expression
-    suite
-
-while True:
-    item = get_next_item()
-    if not item:
-        break
-process_item(item)
-
-
-for variable if iterable:
-    suite
 
 for country in ["Denmark", "Finland", "Norway", "Sweden"]:
     print(country)
-
 countries = ["Denmark", "Finland", "Norway", "Sweden"]
-for country in coutries:
+for country in countries:
     print(country)
 
 for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
@@ -72,3 +50,4 @@ for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
         print(letter, "is a vowel")
     else:
         print(letter, "is a consonant")
+
